c0ee4b81a87e642d8e5e36e18bed44150fc2b73c
havensryan <20.ryan.havens@ksd.org>
2019-02-04 12:20:50 -0800

    AA

---
+++ havensryangit.sh
@@ -0,0 +1,6 @@
+git config --list;
+git config --global user.username  havensryan;
+git config --global user.name  havensryan;
+git config --global user.user havensryan;
+git config --global user.email 20.ryan.havens@ksd.org;
+git config --list;
